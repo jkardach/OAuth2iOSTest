786ffde212d6ab3a7820bab468a993c19dc1b572
Jim Kardach <jim@kardach.clom>
2023-08-29 09:23:32 -0700

    Initial Commit

---
+++ OAuth2iOSTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OAuth2iOSTest
+//
+//  Created by jim kardach on 8/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OAuth2iOSTest/OAuth2iOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OAuth2iOSTestApp.swift
+//  OAuth2iOSTest
+//
+//  Created by jim kardach on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct OAuth2iOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
